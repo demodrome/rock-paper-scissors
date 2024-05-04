44d9eb2007d64c663a0c52ffc00ea12e539dd337
Chris <167183247+demodrome@users.noreply.github.com>
2024-05-04 05:28:32 +0100

    feat: Add function to get the player'schoice of rock, paper, or scissors

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ function getRandomNumber() {
     return Math.floor(Math.random() * 3);
 }
 
+// Game selection options
 const choices = ['rock', 'paper', 'scissors'];
 
 /**
- * Get the computers choice
+ * Get the computer's choice
  * @returns {String} The computers choice of rock, paper, or scissors
  */
 function getComputerChoice() {
@@ -17,4 +18,20 @@ function getComputerChoice() {
 
     // Return the array value at a random index from 0 - 2
     return choices[randomNumber];
+}
+
+/**
+ * Get the player's choice
+ * @returns {String} The player's choice of rock, paper, or scissors
+ */
+function getPlayerChoice() {
+    // Get the player's choice and convert to lowercase to avoid case sensitivity issues
+    const playerChoice = prompt('Rock, Paper, or Scissors?').toLocaleLowerCase();
+
+    // Return an error message if the player's input does not match any value in the choices array
+    if (!choices.includes(playerChoice)) {
+        return 'Invalid input. Please enter either rock, paper, or scissors.';
+    }
+
+    return playerChoice;
 }
