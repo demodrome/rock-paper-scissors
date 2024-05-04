5709f4960e3e23832acb81fe8dc276ccaa12e4c3
Chris <167183247+demodrome@users.noreply.github.com>
2024-05-04 05:16:39 +0100

    feat: Add function to get the computer's choice of rock, paper, or scissors

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function getRandomNumber() {
     return Math.floor(Math.random() * 3);
 }
+
+/**
+ * Get the computers choice
+ * @returns {String} The computers choice of rock, paper, or scissors
+ */
+function getComputerChoice() {
+    const computerChoices = ['rock', 'paper', 'scissors'];
+    const randomNumber = getRandomNumber();
+
+    // Return the array value at a random index from 0 - 2
+    return computerChoices[randomNumber];
+}
